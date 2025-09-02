ed9f2c944b4e5fa897d0c463f414d3139b66d643
olemissnsbe <President.nsbe.um@gmail.com>
2025-09-01 20:36:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const events = [
     date: "Aug 29, 2025",
     time: "6:00–7:30 PM",
     location: "Brewer Hall 205",
-    description: "Kickoff, fall overview, committees sign-up, snacks provided."
+    description: "Kickoff, fall overview, committees sign-up, snacks provided.",
     gcalUrl: "https://calendar.google.com/calendar/ical/25b7b1807f72f7445ee7e893a149ccbcc53a124d83890fd4804ff48085bc53b3%40group.calendar.google.com/public/basic.ics",
   },
   {
